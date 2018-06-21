4d3be4b43f43c0e2ae9c0566eaa6fc2e6d50f992
nerd3d <nerd3d@gmail.com>
2018-06-21 17:51:31 -0600

    Added Teleport Preview Object
    Press to spawn Teleport Preview...
    Release to Teleport

--- CrossRoadVR/Assets/_Scripts/Look_Teleport.cs
+++ CrossRoadVR/Assets/_Scripts/Look_Teleport.cs
@@ -2,16 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Look_Teleport : MonoBehaviour
 {
+    private enum Hand { NONE, LEFT, RIGHT };
+
     [Range(0.0f, 100.0f)] public float _TeleportDistance;
-    public bool _ShowDebug = false;
+    public bool _ShowDebug;
+    public GameObject _TargetPrefab;
 
     private OVRCameraRig _CameraRig = null;
+    private GameObject _TargetInstance = null;
+    private Hand _InHand = Hand.NONE;
 
     // Use this for initialization
     void Start()
     {
+        if (_TargetPrefab == null)
+        {
+            Debug.LogError(name + ": Missing Teleport Preview Prefab");
+            enabled = false;
+            return;
+        }
+
         OVRCameraRig[] CameraRigs = gameObject.GetComponentsInChildren<OVRCameraRig>();
 
         if (CameraRigs.Length == 0)
@@ -25,18 +38,51 @@ public class Look_Teleport : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1") || Input.GetButtonDown("Fire3"))
+        Transform centerEye = _CameraRig.centerEyeAnchor;
+        Vector3 lookDir = centerEye.TransformDirection(Vector3.forward);
+        Vector3 moveDir = new Vector3(lookDir.x, 0, lookDir.z);
+        Vector3 moveLoc = moveDir.normalized * _TeleportDistance + transform.position;
+
+        if (_InHand == Hand.NONE && (Input.GetButtonDown("Fire1") || Input.GetButtonDown("Fire3")) )
         {
+            if (_TargetInstance == null)
+            {
+                _TargetInstance = Instantiate(_TargetPrefab, moveLoc, transform.rotation);
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    _InHand = Hand.RIGHT;
+                }
+                else
+                {
+                    _InHand = Hand.LEFT;
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        if ((_InHand == Hand.RIGHT && Input.GetButtonUp("Fire1")) 
+            || (_InHand == Hand.LEFT && Input.GetButtonUp("Fire3")))
+        {
+            if (_TargetInstance != null)
+            {
+                transform.position = _TargetInstance.transform.position;
+                Destroy(_TargetInstance);
+                _TargetInstance = null;
+                _InHand = Hand.NONE;
+            }
+            else
+            {
+                return;
+            }
+
             if (_ShowDebug)
             {
                 Debug.Log("Teleporting Forward");
             }
 
-            Transform centerEye = _CameraRig.centerEyeAnchor;
-            Vector3 lookDir = centerEye.TransformDirection(Vector3.forward);
-            Vector3 moveDir = new Vector3(lookDir.x, 0, lookDir.z);
-
-            transform.Translate(moveDir.normalized * _TeleportDistance);
         }
     }
 }
