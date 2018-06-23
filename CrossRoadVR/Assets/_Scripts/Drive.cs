fc87af3df7581a7093e4d8bc935c70bfe2e812bb
Nautilus5 <adambennion17@hotmail.com>
2018-06-23 15:08:45 -0600

    VehicleSpawn added

--- CrossRoadVR/Assets/_Scripts/Drive.cs
+++ CrossRoadVR/Assets/_Scripts/Drive.cs
@@ -6,13 +6,17 @@ using UnityEngine;
 public class Drive : MonoBehaviour
 {
     [Range(0.5f, 50.0f)] public float speed = 3.0f;
-    public float xLimit = 32f;
-
+    //public float xLimit = 32f;
+    private float initZ;
+    public float swerveSpeed = 0;
+    private float swerveMax = 1;
+    private bool swerveRight = false;
     private Rigidbody body;
 
     void Start()
     {
         body = GetComponent<Rigidbody>();
+        initZ = transform.position.z;
     }
 
     // Update is called once per frame
@@ -23,9 +27,31 @@ public class Drive : MonoBehaviour
 
         body.velocity = vel;
 
-        if (transform.position.x > xLimit)
+        /*if (transform.position.x > xLimit)
         {
             transform.position = new Vector3( -xLimit, transform.position.y, transform.position.z);
+        }*/
+        Move();
+    }
+    void Move()
+    {
+        if (transform.position.z > initZ + swerveMax)
+        {
+            swerveRight = false;
         }
+        else if (transform.position.z < initZ -swerveMax)
+        {
+            swerveRight = true;
+        }
+
+        if (swerveRight)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + swerveSpeed);
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - swerveSpeed);
+        }
+        gameObject.GetComponent<Rigidbody>().velocity = transform.forward * speed;
     }
 }
---
+++ CrossRoadVR/Assets/_Scripts/VehicleSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleSpawner : MonoBehaviour {
+
+    public float spawnFrequency = 5;
+    public float laneSpeed = 4;
+    [Range(0.0f, 0.01f)]
+    public float swerveSpeed = 0.001f;
+    [Range(0.0f, 10.0f)]
+    public float swerveMax = 1;
+    public GameObject[] spawneable;
+    public float vehicleLifeSpan = 20;
+    private Queue<GameObject> vehicles;
+    private float spawnStartTime = 2f;
+	// Use this for initialization
+	void Start () {
+        vehicles = new Queue<GameObject>();
+        InvokeRepeating("SpawnVehicle", spawnStartTime, spawnFrequency);
+        InvokeRepeating("DestroyVehicle", vehicleLifeSpan+spawnStartTime, spawnFrequency);
+    }
+	
+	//called once per fixed "frame" (computer frame rate will not affect gameplay speed)
+	void FixedUpdate () {
+	}
+    private void SpawnVehicle()
+    {
+        GameObject newVehicle = Instantiate(spawneable[0],transform);
+        //newVehicle.transform.position = transform.position;
+        newVehicle.GetComponent<Drive>().speed = laneSpeed;
+        newVehicle.GetComponent<Drive>().swerveSpeed = swerveSpeed;
+        vehicles.Enqueue(newVehicle);
+    }
+    private void DestroyVehicle()
+    {
+        Destroy(vehicles.Dequeue());
+    }
+}
