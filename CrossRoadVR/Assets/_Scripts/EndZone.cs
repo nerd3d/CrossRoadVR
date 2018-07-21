dc1b3f25c3043e65bcfacac2d36bb30d956361d1
Nautilus5 <adambennion17@hotmail.com>
2018-07-21 13:11:35 -0600

    Endzone prefab and script, Vehicles scene changes.

---
+++ CrossRoadVR/Assets/_Scripts/EndZone.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndZone : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("ENDZONE COLLISION @ " + DateTime.Now);
+        if(collision.gameObject.tag == "Player")
+        {
+            var em = GetComponentInChildren<ParticleSystem>().emission;
+            em.rateOverTime = 50;
+            GetComponent<Collider>().enabled = false;
+        }
+    }
+
+}
