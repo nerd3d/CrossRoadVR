e26d2223a891696db5e0eeaa440c349ebf7e67e1
nerd3d <nerd3d@gmail.com>
2018-07-21 13:52:05 -0600

    Added Player Spawn location and truck death

---
+++ CrossRoadVR/Assets/_Scripts/DeathTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathTrigger : MonoBehaviour
+{
+    public GameObject _PlayerSpawn;
+
+    void OnEnable()
+    {
+        _PlayerSpawn = GameObject.FindGameObjectWithTag("Respawn");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.position = _PlayerSpawn.transform.position;
+        }
+    }
+}
